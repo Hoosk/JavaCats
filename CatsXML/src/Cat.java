2f4b1230289cbbe4d45f3961c546acd600a2ab29
Hoosk <hoosksky@gmail.com>
2014-10-27 17:27:07 +0100

    First Commit

---
+++ CatsXML/src/Cat.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class Cat {	
+	private URL url;
+	private DocumentBuilderFactory dbf;
+	private DocumentBuilder db;
+	private org.w3c.dom.Document doc;
+	private ArrayList<String> arrayGatos = new ArrayList<String>();
+	public Cat(int cantidad) throws IOException {
+		// TODO Auto-generated constructor stub
+		this.url = new URL("http://thecatapi.com/api/images/get?format=xml&results_per_page="+cantidad);
+		url.openConnection();	
+		this.dbf = DocumentBuilderFactory.newInstance();
+		try {
+			this.db = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			this.doc = this.db.parse(url.openStream());
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<String> getCat(){
+		doc.getDocumentElement().normalize();
+		NodeList nodeList = doc.getElementsByTagName("image");
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {				
+				Element eElement = (Element) node;				
+				this.arrayGatos.add(eElement.getElementsByTagName("url").item(0).getTextContent());
+			}
+		}		
+		return this.arrayGatos;		
+	}
+	
+	
+	
+}
---
+++ CatsXML/src/Main.java
@@ -0,0 +1,79 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, ParserConfigurationException {
+		
+		
+		Cat meow = new Cat(10);
+		System.out.println("XML cargado, y array lleno de urls...");
+
+		ArrayList<String> miau = meow.getCat();
+		int purr = 0;		
+		try {
+            String path = miau.get(purr);
+            miau.remove(purr);
+            System.out.println("descargando primer gato desde... " + path);
+            URL url = new URL(path);
+            BufferedImage image = ImageIO.read(url);           
+            
+            JLabel label = new JLabel(new ImageIcon(image));
+            label.addMouseListener(new MouseAdapter()   {   
+
+                public void mouseClicked(MouseEvent e)   
+                {   
+                	
+                	if (!miau.isEmpty()) {
+                		String path = miau.get(purr);
+                    	miau.remove(purr);                	 
+    					try {	                    
+    						URL url = new URL(path);
+    						System.out.println("Cargando siguiente imagen de gato desde..." + path);
+    						
+    						BufferedImage image = ImageIO.read(url);
+    	                    label.setIcon(new ImageIcon(image));
+    					} catch (MalformedURLException e1) {
+    						// TODO Auto-generated catch block
+    						e1.printStackTrace();
+    					} catch (IOException e1) {
+    						// TODO Auto-generated catch block
+    						e1.printStackTrace();
+    					}
+					}else{						
+						System.out.println("Ya no hay mas gatos.");
+					}               	
+                              
+                }   
+            });            
+            JFrame f = new JFrame();            
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.getContentPane().add(label);
+            f.pack();
+            f.setLocation(200, 200);
+            f.setVisible(true);
+            
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+		
+		
+		
+		
+	}
+	
+	
+}
